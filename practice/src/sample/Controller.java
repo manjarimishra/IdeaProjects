67a1f65b7c498b4509069e972250201c76881a6f
manjarimishra <manjari.mishra.cse@gmail.com>
2017-01-10 22:37:12 -0800

    bugFix
    bugFix

--- practice/src/sample/Controller.java
+++ practice/src/sample/Controller.java
@@ -7,7 +7,7 @@ public class Controller {
     public class MyFrame {
         public static void main(String[] args) {
             JFrame frame = new JFrame("Test Frame");
-            frame.setSize(400, 300);
+            frame.setSize(400, 400);
             frame.setVisible(true);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         }
